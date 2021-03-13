8ba5d54662fbaf2d010dbd51093ccbf076e8b386
alhussein mahmoud <tayfoooooor@hotmail.com>
2021-03-13 23:13:58 +0200

    first commit

--- public/index.js
+++ public/index.js
@@ -1,66 +1,221 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    console.log("KHALID");
-    let form = '';
-    let loggedIn = false;
-    let pass = '1234567';
-    if(!loggedIn) {
-        form = $("#compdata").html();
-        $("#compdata").html(`<div id="login" class="row mt-4"><input type="password" class="form-control col-6" id="key" placeholder="كلمة المرور" /><button id="en" class="form-control col-2">دخول</button></div>`);
-        // console.log(form);
-        localStorage.setItem('pass', pass)
-    } else {
-        $("#compdata").html(form);
-    }
+    var responses = { q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0, q7: 0, q8: 0, q9: 0, q10: 0, q11: 0, q12: 0, q13: 0, q14: 0, q15: 0, q16: 0, q17: 0, q18: 0, q19: 0, q20: 0 };
+    var valid = {name: false, q: false};
+    var questions = {};
 
-    $(document).on("click", "#save", function() {
-        let name = $("#name").val();
-        let phone = $("#phone").val();
-        let type = $("input[name=type]:checked").val();
-        let status = $("input[name=status]:checked").val();
-        let address = $("#address").val();
-        console.log({name, phone, type, address, status});
+    $.ajax({
+        type: "GET",
+        url: "http://192.168.2.224:3000/quest",
+        dataType: "html",
+        // crossDomain:true,
+        success: function (res) {
+            res = $.parseJSON(res);
+            let html = '';
+            $.each(res, (index, row) => {
+                // console.log(row);
+                questions[`q${row.id}`] = row.text;
+                html += `<div class="question my-3"> <span class="q${row.id} text-primary">${row.text}${scales(row.id)}</span></div>`;
+            });
+            html += `<hr/><div class="row">
+                    <span class="align-top col-3">مقترحات لتحسين مستوى الإنتاجية داخل الشركة (في حالة أكثر من واحد كل مقترح في سطر جديد):</span>
+                    <textarea rows="4" id="sugg" class="col-6"></textarea>
+                    </div>
+                    <hr />
+                    <button id="submit" type="button" class="form-control btn btn-primary">إرسال</button>
+                    `;
+            $("#questions").html(html);
+        }
+    }); //Ajax get questions
+    var name = '';
+    $(document).on('blur', "#empid", function () {
+        if ($("#empname").hasClass("alert-danger")) $("#empname").removeClass("alert-danger");
+        let id = $(this).val();
+        if (id == '') id = 0;
+        let url = "http://192.168.2.224:3000/emp/" + id;
+        console.log(url);
         $.ajax({
-            url: "http://192.168.2.224:3001/save",
-            type: "post",
-            data: {type, status, name, phone, address},
-            success: function(result) {
-                console.log(result.affectedRows);
-                $("#name").val('');
-                $("#phone").val('');
-                $("#address").val('');
+            url,
+            type: "GET",
+            crossDomain: true,
+            success: data => {
+                let tmp = '';
+                if (data == "exists") {
+                    if (!$("#empname").hasClass("alert-danger")) $("#empname").addClass("alert-danger");
+                    tmp = 'هذا الرقم قام بتعبئة الإستبيان من قبل,,, شكراً';
+                    valid.name = false;
+                    //$("#submit").prop("disabled", "true");
+                }
+                else {
+                    tmp = 'رقم الموظف غير صحيح';
+                    if (data.name == null) {
+                        valid.name = false;
+                        //$("#submit").prop("disabled", "true");
+                        if (!$("#empname").hasClass("alert-danger")) $("#empname").addClass("alert-danger");
+                    } else {
+                        if (data.name) tmp = data.name;
+                        name = tmp;
+                        if (data.dir) tmp += ': ' + data.dir
+                        valid.name = true;
+                    }
+                }
+                $("#empname").html(tmp);
+            }
+        });
+    });
+
+    $(document).on("click", "span", function () {
+        let i = $(this);
+        let q = i.data("qno");
+        let res = i.data("val");
+        responses[q] = res; // save response
+        // console.log(responses);
+
+        let parent = i.parent(); //div that contains emojies :)
+        parent.children().each(function () {
+            if ($(this).hasClass("selected"))
+                $(this).removeClass("selected");
+        });
+        i.addClass("selected");
+
+        let qtext = parent.parent(); //console.log(qtext);
+        if (qtext.hasClass("alert-danger")) qtext.removeClass("alert-danger");
+
+        let count = 0;
+        $.each(responses, function (k, v) {
+            if (v == 0) {
+                count++;
             }
         })
-    });
-
-    $(document).on('click', '#en', function() {
-        let pass = localStorage.getItem('pass');
-        if(pass == $("#key").val()) {
-            $("#compdata").html(form);
+        if (count) {
+            valid.q = false;
+            console.log(count);
+            //$("#submit").prop("disabled", "true");
         } else {
-            alert("Wrong");
+            valid.q = true;
         }
+
     });
 
-    $(document).on("click", "#addLink", function() {
-        $("#compdata").html("AddLink is clicked");
-    });
-
-    $(document).on('keydown', '#phone', function(e){
-        let keyCode = e.keyCode;
-        let temp = '';
-        let count = 0;
-        let phones = [];
-        if(keyCode < 106 && keyCode > 96) {
-            temp += char(keyCode);
-            if(temp.length == 10) {
-                phones.push(temp);
-                temp = '';
+    $(document).on("click", "#submit", function () {
+        console.log(valid);
+        $.each(responses, function (k, v) {
+            if (v == 0) {
+                console.log(k);
+                valid.q = false;
+                $("." + k).addClass("alert-danger");
             }
-        } else {
-            console.log(temp);
+        });
+        if (valid.q && valid.name) {
+            let sugg = $("#sugg").val();
+            let empid = $("#empid").val();
+            if (sugg !== "") responses['sugg'] = sugg;
+            if (empid !== "") responses['empid'] = empid;
+
+            console.log(responses);
+            $.ajax({
+                type: "post",
+                url: "http://192.168.2.224:3000/save",
+                data: { all: JSON.stringify(responses) },
+                success: function (data) {
+                    console.log(data);
+                    if(data =="done") {
+                        $("span .bi").each(function() {
+                            if($(this).hasClass("selected")) {
+                                $(this).removeClass("selected");
+                                $("#empid").val("");
+                                $("#sugg").val("");
+                                $("#empname").html("");
+                            }
+                        })
+                        // alert(`شكراً ${name} تم الحفظ بنجاح`);
+                        $(".jumbotron").html(`<p class="alert alert-success">شكراً ${name} تم الحفظ بنجاح<i class="bi bi-heart-fill"></i></p>`);
+                        $("#questions").css('display', 'none');
+                    }
+                }
+            }) // ajax
         }
-        console.log(phones);
     });
 
-});
+    $(document).on("click", '#rows', function() {
+        $(".jumbotron").css('display', 'none');
+        $.ajax({
+            url: "http://192.168.2.224:3000/rows",
+            type: "get",
+            dataType: "json",
+            success: function(rows) {
+                let total = 0;
+                let table = '<table class="table table-sm table-stripped"><tr><th>الإدارة</th><th>العدد</th></tr>';
+                rows.forEach(elem => {
+                    table += `<tr><td>${elem.gdir}</td><td>${elem.rows}</td></tr>`;
+                    total += elem.rows;
+                });
+                table += `<tr><th>إجمــالي الإستبيانات </th><th>${total}</th></tr>`;
+                table += `</table>`;
+                Object.keys(responses).forEach(q => {
+                    table += `UPDATE data SET ${q} = 3 WHERE ${q} > 3;<br>`;
+                })
+                $("#questions").html(table);
+            }
+        });
+    });
+
+    $(document).on("click", '#results', function() {
+        $(".jumbotron").css('display', 'none');
+        $.ajax({
+            url: "http://192.168.2.224:3000/results",
+            type: "get",
+            dataType: "json",
+            success: function(rows) {
+                // console.log({questions, rows})
+                let table = '<table class="table table-sm table-stripped"><tr><th>المعيار</th><th>نسبة الرضا</th></tr>';
+                let total_sum = 0;
+                Object.keys(rows).forEach(qno => {
+                    let tmp = rows[qno]; //actual response avg value
+                    total_sum += +tmp
+                    table += `<tr><td>${questions[qno]}</td><td>${Math.round(tmp * 33.333)}%</td></tr>`;
+                });
+                console.log({total_sum})
+                table += `<tr><th>نسبة الرضا الكلية</th><th>${Math.round(total_sum)}%</th></tr>`;
+                table += `</table>`;
+
+                $("#questions").html(table);
+            }
+        });
+    });
+
+    $(document).on("click", '#suggs', function() {
+        $(".jumbotron").css('display', 'none');
+        $.ajax({
+            url: "http://192.168.2.224:3000/suggs",
+            type: "get",
+            dataType: "json",
+            success: function(rows) {
+                suggs = [];
+                rows.forEach(row => {
+                    console.log(row.sugg);
+                    let temp = row.sugg.split('\n');
+                    temp.forEach(tmp => suggs.push(tmp));
+                })
+                let table = '<table class="table table-sm table-stripped"><tr><th>المقترحات</th></tr>';
+                suggs.forEach(elem => {
+                    table += `<tr><td>${elem}</td></tr>`;
+                });
+                table += `</table>`;
+                $("#questions").html(table);
+            }
+        });
+    });
+
+}) //ready
+
+
+function scales(myclass) {
+    return `
+    <div class="scales">
+        <span data-qno="q${myclass}" data-val="1" class="bi bi-emoji-angry"></span>
+        <span data-qno="q${myclass}" data-val="2" class="bi bi-emoji-neutral"></span>
+        <span data-qno="q${myclass}" data-val="3" class="bi bi-emoji-heart-eyes"></span>
+    </div>
+    `;
+}
--- server.js
+++ server.js
@@ -1,47 +1,112 @@
-var express = require("express");
-var mysql = require("mysql2");
-var bodyPaser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const mysql = require("mysql2");
 
-var app = express();
-app.use(express.static('./public'));
+const app = express();
 
-app.use(bodyPaser.urlencoded());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 var connection = mysql.createConnection({
-    user: "root",
-    password: "formatshort",
-    database: "comp",
-    host: "localhost"
+  host: 'localhost',
+  user: 'root',
+  password: 'formatshort',
+  database: 'quest',
 });
 
-app.get("/", (req, res) => {
-    res.send("hhhhhhhhh");
+// connection.connect();
+// connection.end();
+
+app.use('/emp/:id', (req, res) => {
+  let empid = req.params.id;
+  const sql1 = `SELECT empid FROM data WHERE empid = ?`;
+  connection.query(sql1, [empid], function (err, result, fel) {
+    if (err) {
+      console.log(err);
+      res.send(err)
+    }
+    else {
+      if (result.length) {
+        res.send("exists");
+      }
+      else {
+        const sql2 = `SELECT * FROM emps WHERE id = '${empid}'`;
+        console.log(sql2);
+        connection.query(sql2, function (error, results, fields) {
+          if (error) throw error;
+          console.log('The solution is: ', results[0]);
+          res.header("Access-Control-Allow-Origin", "*");
+          res.send(results[0]);
+        });
+      }
+    }
+  });
+
 });
 
-app.get("/comp", (req, res) => {
-    connection.query(`SELECT * FROM comps`, (err, rows) => {
-        if(err) console.log(err);
-        else res.send(rows);
-    })
-});
-
-app.get("/comp/:id", (req, res) => {
-    connection.query(`SELECT * FROM comps WHERE id = ${req.params.id}`, (err, rows) => {
-        if(err) console.log(err);
-        else res.send(rows);
-    }) 
-});
-
+app.get('/quest', (req, res) => {
+  const sql = `SELECT * FROM questions`;
+  connection.query(sql, (err, rows, fields) => {
+    if (err) console.log(err);
+    res.send(rows);
+  })
+})
 
 app.post("/save", (req, res) => {
-    console.log(req.body);
-    let {name, phone, type, address} = req.body;
-    let sql = `INSERT INTO comps(id, name, phone, type, address) VALUES(null, "${name}", "${phone}", "${type}", "${address}")`
-    connection.query(sql, (err, result) => {
-        if(err) console.log(err);
-        else res.send(result);
-    })
-    // res.send("recieved...");
+  let data = JSON.parse(req.body.all);
+  console.log(data);
+  // let values = Object.values(data);
+  if(!data.sugg) data.sugg = "";
+  // const obj = { a: 5, b: 7, c: 9 }
+  let values = [];
+  let keys = [];
+  Object.entries(data).forEach(([key, value]) => {
+    // console.log(`${key} ${value}`); // "a 5", "b 7", "c 9"
+    keys.push(key);
+    values.push(value);
+  });
+
+  let sql = `INSERT INTO data (${keys.join(',')}) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+  console.log({ sql, values });
+
+  connection.query(sql, values, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    }
+    else {
+      res.send("done");
+    }
+  })
 });
 
-app.listen(3001, () => console.log("SERVER IS RUNNING..."));
+
+app.get("/rows", (req, res) => {
+  let sql = `SELECT gdir, count(*) as 'rows' FROM data JOIN emps on emps.id = empid GROUP BY gdir`;
+  connection.query(sql, (err, rows) => {
+    res.send(rows);
+  });
+});
+
+app.get("/results", (req, res) => {
+  let qs = [];
+  for(let i = 1; i < 21; i++) qs.push(`avg(q${i}) as q${i}`);
+  let sql = `SELECT ${qs.join(', ')} FROM data`;
+  console.log(sql);
+  connection.query(sql, (err, rows) => {
+    res.send(rows[0]);
+  });
+});
+
+
+app.get("/suggs", (req, res) => {
+  let sql = `SELECT sugg FROM data WHERE sugg <> ""`;
+  connection.query(sql, (err, rows) => {
+    res.send(rows);
+  });
+});
+
+app.listen(3000, () => console.log("server is running..."));
